ff409cb9bc53479687fb87b51e6f4334ab454480
minecrawler <me@marco-alka.de>
2017-07-18 10:56:58 +0200

    fix new structure

--- src/Plugin.init.c.js
+++ src/Plugin.init.c.js
@@ -4,5 +4,5 @@ const libs = require('node-mod-load')('SHPS4Node-plugin').libs;
 const Result = require('rustify-js').Result;
 
 libs.meth.init = function() {
-    return Result.fromSuccess(new libs['plugin.h']());
+    return Result.fromSuccess(new libs['Plugin.h']());
 };
